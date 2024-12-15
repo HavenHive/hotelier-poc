1d8e4821c6b504f46233bbbbe1dc4fcab961cd9f
owenEEfobi <jenukohaefobi@gmail.com>
2024-12-15 11:00:22 +0100

    :bug: fixed relative url without baseurl in auth calls

--- lib/auth.client.ts
+++ lib/auth.client.ts
@@ -5,6 +5,10 @@ import {
 } from "better-auth/client/plugins";
 import { createAuthClient } from "better-auth/react";
 const authClient = createAuthClient({
+	baseURL:
+		process.env.NODE_ENV === "development"
+			? "localhost:3000"
+			: "https://hotelier-wine.vercel.app",
 	plugins: [adminClient(), emailOTPClient(), organizationClient()],
 });
 
